b197ea675bf9802b820aeeb6845c4523be707e81
Mathieu BRUN <mathieubrun@users.noreply.github.com>
2017-12-22 12:46:28 +0100

    Added projects

---
+++ src/App/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using Library;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+
+            var services = new ServiceCollection();
+
+            services.AddOptions();
+
+            var config = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine(AppContext.BaseDirectory, "log.txt"))
+                .MinimumLevel.Debug();
+
+            Log.Logger = config.CreateLogger();
+
+            services.AddLogging(log => log.AddSerilog(dispose: true));
+
+            services.Configure<SampleSettings>(configuration);
+            services.AddTransient<SampleClass>();
+
+            using (var p = services.BuildServiceProvider())
+            {
+                var c = p.GetService<SampleClass>();
+
+                c.Go();
+            }
+
+            Console.WriteLine("press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Library/SampleClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Library
+{
+    public class SampleClass
+    {
+        private readonly SampleSettings _settings;
+        private readonly ILogger _logger;
+
+        public SampleClass(IOptions<SampleSettings> options, ILogger<SampleClass> logger)
+        {
+            _settings = options.Value;
+            _logger = logger;
+        }
+
+        public void Go()
+        {
+            _logger.LogDebug($"ComputerName : {_settings.ComputerName}");
+            _logger.LogInformation($"ConfigValue : {_settings.ConfigValue}");
+        }
+    }
+}
